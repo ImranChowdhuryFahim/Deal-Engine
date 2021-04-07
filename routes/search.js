27bbe3ae5a2fa1717560ae31ef9d8bc54c32f456
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-04-08 00:01:30 +0600

    baseurl removed

--- routes/search.js
+++ routes/search.js
@@ -5,6 +5,6 @@ const controller = require('../controllers/search');
 const router = express.Router();
 
 router.route('/deal-search/aus').get(controller.getSearchResult);
-router.route('/').get(controller.index);
+// router.route('/').get(controller.index);
 
 module.exports = router;
