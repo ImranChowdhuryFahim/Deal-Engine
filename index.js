679e32943fe3ed3b165183928abfd16a58e8d274
ImranChowdhuryFahim <imran.cuet.cse17@gmail.com>
2021-05-17 11:31:09 +0600

    db connected

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
-const cors = require('cors');
-
-const search = require('./routes/search')
+const cors = require("cors");
+const mysql = require("mysql");
+const search = require("./routes/search");
 
 const app = express();
 
 app.use(cors());
 
-app.use(search)
+app.use(search);
 
-app.listen(process.env.PORT || 8000);
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+
+  app.listen(process.env.PORT || 8000, () => {
+    console.log("Connected!");
+    con.query("CREATE DATABASE IF NOT EXISTS deal_engine;")
+    con.query("use deal_engine;")
+  });
+});
+
+// if it fails to connect to mysql run these commands.
+// mysql > ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';
+// mysql > FLUSH PRIVILEGES;
+// probable reason permission issue
