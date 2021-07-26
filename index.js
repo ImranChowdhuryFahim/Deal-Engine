57861244ba6044a64178ac653f51681f3e1261c2
Abdul-Moeid <mdmoeid@gmail.com>
2021-07-26 16:54:23 +0600

    models

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const db = require("./models")
+const db = require("./models");
 const search = require("./routes/search");
 const trackProduct = require("./routes/trackProducts");
 const createProduct = require("./routes/createProducts");
@@ -9,19 +9,14 @@ const app = express();
 
 app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(search);
 app.use(trackProduct);
 app.use(createProduct);
 
-db.sequelize.sync().then(()=>{
-    app.listen(process.env.PORT | 8000, ()=>{
-    console.log("server started")
-  })
-})
-
-
-
-
-
+db.sequelize.sync().then(() => {
+    app.listen(process.env.PORT | 8000, () => {
+        console.log("server started");
+    });
+});
