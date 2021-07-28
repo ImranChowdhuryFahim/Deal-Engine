70ce73891d7b69340c064c458bb5ded3f630ee53
Abdul-Moeid <mdmoeid@gmail.com>
2021-07-28 21:52:01 +0600

    newmodel product details

---
+++ websiteModel/productDetailsModel.js
@@ -0,0 +1,33 @@
+module.exports = {
+    productDetailsModel: [{
+            websiteName: "amazon",
+            productLink: `link ager model thake ekhane ashbe`,
+            productDescriptionClass: "a-section a-spacing-medium a-spacing-top-small",
+            imageTag: "img",
+            productShortDtailsClass: "a-row a-expander-container a-expander-extend-container",
+            priceTag: "a-size-medium a-color-price priceBlockBuyingPriceString",
+            titleTag: "a-size-large product-title-word-break",
+            suffix: "",
+        },
+        {
+            websiteName: "jbhifi",
+            productLink: `link ager model thake ekhane ashbe`,
+            productDescriptionClass: "descriptions",
+            imageTag: "img",
+            productShortDtailsClass: "product-keyfeatures",
+            priceTag: "sale",
+            titleTag: "<h1>",
+            suffix: "",
+        },
+        {
+            websiteName: "bigw",
+            productLink: `link ager model thake ekhane ashbe`,
+            productDescriptionClass: "TabContent active-true product-details",
+            imageTag: "img",
+            productShortDtailsClass: "TabContent active-true product-specifications",
+            priceTag: "Price variant-huge",
+            titleTag: "product-header",
+            suffix: "",
+        },
+    ],
+};
